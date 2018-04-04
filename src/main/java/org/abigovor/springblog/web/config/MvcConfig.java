df9951ab7427e35defe523b0bc71625647e0d0d4
Abigo <Abigo>
2018-04-04 14:53:12 +0300

    in memory security config

---
+++ src/main/java/org/abigovor/springblog/web/config/MvcConfig.java
@@ -0,0 +1,17 @@
+package org.abigovor.springblog.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/home").setViewName("home");
+    }
+}
