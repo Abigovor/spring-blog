690e93a2ab3572db687f9960f619b3255460b41f
Abigo <Abigo>
2018-04-04 15:20:25 +0300

    fake user for in-memory check

--- src/main/java/org/abigovor/springblog/SpringBlogApplication.java
+++ src/main/java/org/abigovor/springblog/SpringBlogApplication.java
@@ -1,12 +1,26 @@
 package org.abigovor.springblog;
 
+import org.abigovor.springblog.domain.User;
+import org.abigovor.springblog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBlogApplication {
+public class SpringBlogApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBlogApplication.class, args);
-	}
+    @Autowired
+    private UserService userService;
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBlogApplication.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        User alex = new User("alex@tr.com", "123");
+        userService.save(alex);
+    }
 }
